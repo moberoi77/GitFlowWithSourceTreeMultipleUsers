a2a170b1704cd21a2bade0caeee3f50ca4125ed1
Manjit Oberoi <manjit.oberoi@ge.com>
2017-07-07 19:10:52 +0530

    Merged Conflicts

--- Feature2.java
+++ Feature2.java
@@ -1,13 +1,8 @@
 public class Feature2{
 	public void Feature2(){
-<<<<<<< HEAD
-		System.out.println("Dev1 added... ");
-		System.out.println("Dev2.1 added... ");
-		System.out.println("Dev2.2 added... ");
-=======
 		System.out.println("Dev1.1 added... ");
 		System.out.println("Dev1.2 added... ");
-		System.out.println("Dev2 added... ");
->>>>>>> Dev1 added SOP as part of Feature 1.3
+		System.out.println("Dev2.1 added... ");
+		System.out.println("Dev2.2 added... ");
 	}
 }

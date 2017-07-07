60bc6619cc51e76bde3af3811022b66bd5a7dc52
Manjit Oberoi <manjit.oberoi@ge.com>
2017-07-07 19:01:41 +0530

    Dev2 added SOP as part of Feature 2.3

--- Feature1.java
+++ Feature1.java
@@ -1,6 +1,7 @@
 public class Feature1{
 	public void Feature1(){
 		System.out.println("Dev1 added... ");
-		System.out.println("Dev2 added... ");
+		System.out.println("Dev2.1 added... ");
+		System.out.println("Dev2.2 added... ");
 	}
 }
